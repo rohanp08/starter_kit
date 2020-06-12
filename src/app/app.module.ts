104fd3199500c9a24ea83428e5f4f329687fef57
RohanSanjayPawar <rohan.sanjaypawar@accoliteindia.com>
2020-06-12 13:57:45 +0530

    An angular ethereum starter project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,69 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatInputModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatSortModule,
+  MatIconModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatCardModule,
+  MatGridListModule,
+  MatDialogModule,
+  MatTableModule,
+  MatMenuModule,
+  MatFormFieldModule,
+  MatOptionModule,
+  MatSelectModule,
+  MatCheckboxModule,
+  MatTabsModule,
+  MatListModule,
+  MatSidenavModule
+} from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxWebstorageModule } from 'ngx-webstorage';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSortModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatGridListModule,
+    MatDialogModule,
+    MatTableModule,
+    MatMenuModule,
+    MatFormFieldModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatTabsModule,
+    MatListModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    MatSidenavModule,
+    HttpClientModule,
+    NgxWebstorageModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
